73aad0624e64286cf8ce95319726563a7c722864
gmcyber <37155987+gmcyber@users.noreply.github.com>
2022-11-18 15:45:27 -0500

    deployer

--- src/scripts/base-vms/windows/windows-prep.ps1
+++ src/scripts/base-vms/windows/windows-prep.ps1
@@ -1,13 +1,13 @@
-dd-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 # a good time to complete via remote ssh
 Set-Service -Name sshd -StartupType 'Automatic'
 Set-ItemProperty "HKLM:\Software\Microsoft\Powershell\1\ShellIds" -Name ConsolePrompting -Value $true
 New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
-Write-Host "Create a deployer user: Enter Password"
-$pw = Read-Host -AsSecureString
-New-LocalUser -Name deployer -Password $pw -AccountNeverExpires -PasswordNeverExpires:$true
-Add-LocalGroupMember -Group Administrators -Member deployer
+#Write-Host "Create a deployer user: Enter Password"
+#$pw = Read-Host -AsSecureString
+#New-LocalUser -Name deployer -Password $pw -AccountNeverExpires -PasswordNeverExpires:$true
+#Add-LocalGroupMember -Group Administrators -Member deployer
 Write-Host "Pull down unattend.xml and then sysprep the box"
 wget https://raw.githubusercontent.com/gmcyber/RangeControl/main/src/scripts/base-vms/windows/unattend.xml -Outfile C:\Unattend.xml
 C:\Windows\System32\Sysprep\sysprep.exe /oobe /generalize /unattend:C:\unattend.xml
