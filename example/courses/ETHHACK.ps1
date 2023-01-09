2248ccde3b154b5469506bdc440ebf1d1de7966f
gmcyber <devin@greenmounatincyber.com>
2023-01-09 06:28:04 -0500

    clone names

--- example/courses/ETHHACK.ps1
+++ example/courses/ETHHACK.ps1
@@ -11,12 +11,24 @@ if($initialize_course -eq 'y')
 $rangecontrol = [RangeControl]::new($configuration_file,$roster)
 
 # Note, Students do not need personal networks in this class.  Kali lands on the COURSE-WAN and picks up a DHCP address
+<#
+These are the Base VMs
+kali.2022.4.base
+pf.2.6.0.base
+rocky.9.1.base
+server.2019.base.v2
+vyos.1.4.base
+windows10.ltsc.base
+xubuntu.20.04.base.v2
+ubuntu.22.04.1.base
 
+
+#>
 $deploy_vms = Read-Host "Do you want to deploy the VMs (y/n)"
 if($deploy_vms -eq 'y')
 {
     #STUDENTS
-    $rangecontrol.DeployClones("kali.2022.4.f22","kali01")
-    $rangecontrol.DeployClones("windows.10.ltsc.f22.v2","win10-02")
+    $rangecontrol.DeployClones("kali.2022.4.base","kali01")
+    $rangecontrol.DeployClones("windows10.ltsc.base","wks01")
 }
 
